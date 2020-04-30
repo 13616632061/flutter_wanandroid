4b4c850d7b3817942c682f08b410ec252c12207f
13616632061 <13616632061@139.com>
2020-04-30 17:41:25 +0800

    init

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/thl/Library/Android/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/thl/AndroidStudioProjects/smzc workspace/flutter_wanandroid"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=D:\Fullter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\FlutterProject\flutter_wanandroid"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/thl/Library/Android/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.2.2"
-export "FLUTTER_BUILD_NUMBER=0.2.2"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Fullter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=0.2.5"
+export "FLUTTER_BUILD_NUMBER=25"
